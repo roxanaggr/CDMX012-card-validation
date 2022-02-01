35201ca04c99d01c4b12c3bb3877587a5ab23082
Roxana Gonzalez <roxanagonzalez@MacBook-Pro-de-Roxana.local>
2022-01-31 23:09:36 -0600

    Funcion validator y maskify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
+//Ingresar numero de 16 digitos sin espacios
+//Si el número ingresado es de 16 digitos, desbloquear el botón continuar
+//Guardar los 16 digitos ingresados
+//Si se ingresa un número menor a 16 digitos, bloquear el botón y mostrar una alerta.(Ingresa tu número de 16 digitos para continuar)
+//Si no se ingresa ningun número, bloquear el botón y mostrar una alerta. (Ingresa tu número de 16 digitos para continuar)
 
+//Esconder con un # los primeros 12 digitos
+//Mostrar los últimos 4 digitos
 
-const button = document.querySelector('botonValidar')
-button.disabled = true;
+//let btnNext = document.getElementById("botonContinuar");
+//btnNext.addEventListener("click", function () {
+// document.location.href = "valida.html";
+//});
 
+//No aceptar un input en blanco y solo aceptar números
+//if (digitos ==="" || /[^0-9|/s]/){
+//console.log (digitos.length)
+//alert ("¡Debes de ingresar un número válido!");
+//}
+
+//Debe de ser mayor a 13 carácteres
+//if(digitos.length < 13 || digitos.lenght > 19){
+//alert ("¡Debes de ingresar un número válido!");
+//}
 
 import validator from './validator.js';
-console.log(validator);
+
+let btnGuardarInput=document.getElementById("botonValidar");
+let input=document.getElementById("numInput");
+
+
+btnGuardarInput.addEventListener("click", function (){
+   let mask = (validator.maskify(input.value));
+   if (validator.isValid(input.value)===true){
+      document.getElementById("msgExito").innerHTML = "Siiii";
+      input.value = mask;
+   }else
+   document.getElementById("msgError").innerHTML = "Noooo";
+   input.value = mask;
+
+   //console.log (validator.isValid(input.value))
+   //console.log (validator.maskify(input.value));
+
+});
+
+
+
+   
+   
+
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
-};
+  
+  
+  isValid: (digitos) => {
 
-export default validator;
+    //convertir string en Array
+    let numerosArray = digitos.split('');
+    //invertir Array
+    let invertirNum = numerosArray.reverse();
+    //declara la suma de todos los elementos del array comenzando en 0
+    let suma = 0;
+
+    //identificar la longitud del array
+    for (let i=0; i < invertirNum.length; i++){
+      //convierte a numeros el array
+      invertirNum[i] = parseInt(invertirNum[i]);
+      //Si el resto de dividir i entre dos es cero significa que i es par,
+      if (i%2===1){
+        //entonces multiplica el numero de i x 2, conviertelo a string, conviertelo a numeros, separa los numeros de dos digitos y sumalos
+        invertirNum[i] = (invertirNum[i] *2).toString().split("").map(Number).reduce((prev, curr) => prev + curr, 0);
+        }
+      //suma todos
+      suma += invertirNum[i]
+    }
+
+    if (suma % 10 === 0){
+      return true;
+      } else
+      return false;
+  },
+  
+  maskify: function (digitos) {
+
+    let ultimosCuatro = digitos.slice(-4);
+    const valorArray = digitos.split('');
+    for (let i = 0; i < valorArray.length; i++) {
+      valorArray[i] = valorArray[i].replace(/[0-9]/, '#');
+    }
+    let arrayString = valorArray.join('');
+    let masked = arrayString.slice(0, -4) + ultimosCuatro;
+    return masked; 
+    } 
+
+}
+
+export default validator
